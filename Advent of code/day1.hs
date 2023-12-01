3242a7d19cad846f2ddfadd72ece3991786e0f82
Daniel Agyapong <kunkumah@gmail.com>
2023-12-02 00:05:16 +0100

    update code for part 2 of day 1

--- Advent of code/day1.hs
+++ Advent of code/day1.hs
@@ -12,15 +12,15 @@ getFirstandLastDigits str = [head digits, last digits]
 repl :: String -> String
 repl "" = ""
 repl str = case str of
-    _ | "one" `isPrefixOf` str -> '1' : repl (drop 3 str)
-    _ | "two" `isPrefixOf` str -> '2' : repl (drop 3 str)
-    _ | "three" `isPrefixOf` str -> '3' : repl (drop 5 str)
-    _ | "four" `isPrefixOf` str -> '4' : repl (drop 4 str)
-    _ | "five" `isPrefixOf` str -> '5' : repl (drop 4 str)
-    _ | "six" `isPrefixOf` str -> '6' : repl (drop 3 str)
-    _ | "seven" `isPrefixOf` str -> '7' : repl (drop 5 str)
-    _ | "eight" `isPrefixOf` str -> '8' : repl (drop 5 str)
-    _ | "nine" `isPrefixOf` str -> '9' : repl (drop 4 str)
+    _ | "one" `isPrefixOf` str -> '1' : repl (drop 2 str)
+    _ | "two" `isPrefixOf` str -> '2' : repl (drop 2 str)
+    _ | "three" `isPrefixOf` str -> '3' : repl (drop 4 str)
+    _ | "four" `isPrefixOf` str -> '4' : repl (drop 3 str)
+    _ | "five" `isPrefixOf` str -> '5' : repl (drop 3 str)
+    _ | "six" `isPrefixOf` str -> '6' : repl (drop 2 str)
+    _ | "seven" `isPrefixOf` str -> '7' : repl (drop 4 str)
+    _ | "eight" `isPrefixOf` str -> '8' : repl (drop 4 str)
+    _ | "nine" `isPrefixOf` str -> '9' : repl (drop 3 str)
     _ -> head str : repl (tail str)
 
 getCalibrationSumPart1 :: [String] -> Int
@@ -29,10 +29,9 @@ getCalibrationSumPart1 = sum . map (read . concatMap show . getFirstandLastDigit
 getCalibrationSumPart2 :: [String] -> Int
 getCalibrationSumPart2 = sum . map (read . concatMap show . getFirstandLastDigits . repl)
 
--- Part 2 not fully working
 main :: IO()
 main = do
     input <- readLines "Advent of code/Input/day1Input.txt"
     print $ getCalibrationSumPart1 input
     print $ getCalibrationSumPart2 input
-    print $ repl "14nsnlqqlgfourxbzzxfztvbxsnxttjmktcxkkkzfphppsczqoneightgc"
+    print $ repl "iowu27dgdisi3eighthree"
